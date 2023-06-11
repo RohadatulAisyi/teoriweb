993e1a0b38fa7a4259365e5a61285280758105f7
A. Nur Rohadatul Aisyi Harun <radaasy@gmail.com>
2023-06-11 18:04:55 +0700

    teoriweb

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,3 +1,4 @@
+<!--Teori--> 
 <?php
 
 /** @var yii\web\View $this */
@@ -9,6 +10,7 @@ use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
+use yii\helpers\Url;
 
 AppAsset::register($this);
 
@@ -24,30 +26,89 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
     <title><?= Html::encode($this->title) ?></title>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <title>Knight Bootstrap Template - Index</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+        <!-- Favicons -->
+        <link href="templateteori/img/favicon.png" rel="icon">
+        <link href="templateteori/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="templateteori/vendor/aos/aos.css" rel="stylesheet">
+        <link href="templateteori/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="templateteori/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="templateteori/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="templateteori/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="templateteori/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="templateteori/css/style.css" rel="stylesheet">
     <?php $this->head() ?>
 </head>
-<body class="d-flex flex-column h-100">
+
+<body>
+
 <?php $this->beginBody() ?>
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'Mahasiswa', 'url' => ['/mahasiswa105/about']],
-        ]
-    ]);
-    NavBar::end();
-    ?>
-</header>
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+    <div class="hero-container">
+      <a href="index.html" class="hero-logo" data-aos="zoom-in"><img src="templateteori/img/hero-logo.png" alt=""></a>
+      <h1 data-aos="zoom-in">Welcome To Knight Studios</h1>
+      <h2 data-aos="fade-up">We are team of talented designers making websites with Bootstrap</h2>
+      <a data-aos="fade-up" data-aos-delay="200" href="#about" class="btn-get-started scrollto">Get Started</a>
+    </div>
+</section><!-- End Hero -->
 
-<main id="main" class="flex-shrink-0" role="main">
+  <!-- ======= Header ======= -->
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <a href="index.html"><img src="templateteori/img/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
+
+<main>
     <div class="container">
         <?php if (!empty($this->params['breadcrumbs'])): ?>
             <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
@@ -57,14 +118,64 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     </div>
 </main>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
-        </div>
+   <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+<div class="footer-top">
+
+  <div class="container">
+
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <a href="#header" class="scrollto footer-logo"><img src="templateteori/img/hero-logo.png" alt=""></a>
+        <h3>Knight</h3>
+        <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      </div>
     </div>
-</footer>
+
+    <div class="row footer-newsletter justify-content-center">
+      <div class="col-lg-6">
+        <form action="" method="post">
+          <input type="email" name="email" placeholder="Enter your Email"><input type="submit" value="Subscribe">
+        </form>
+      </div>
+    </div>
+
+    <div class="social-links">
+      <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+      <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+      <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+    </div>
+
+  </div>
+</div>
+
+<div class="container footer-bottom clearfix">
+  <div class="copyright">
+    &copy; Copyright <strong><span>Knight</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/knight-free-bootstrap-theme/ -->
+    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+  </div>
+</div>
+</footer><!-- End Footer -->
+
+<!-- Vendor JS Files -->
+    <script src="templateteori/vendor/aos/aos.js"></script>
+    <script src="templateteori/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="templateteori/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="templateteori/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="templateteori/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="templateteori/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+    <script src="templateteori/js/main.js"></script>
 
 <?php $this->endBody() ?>
 </body>
